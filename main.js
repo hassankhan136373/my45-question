dd6a93271a86e240fd4a95e3b99a5e3a3eed54d5
hassankhan136373 <164183678+hassankhan136373@users.noreply.github.com>
2024-03-21 15:59:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//camelCase
+// snake_case
+//pascalCase
+var userName = 'hassan'; // e.g. camelCase
+var user_name = 'ahsan'; // e,g. snake case
+var UserName = 'zeeshan'; // e.g. pascalCase
+var isueserExist = true;
+var is_ueser_Exist = false;
+var IsueserExist = 1;
+var anytype;
+//asseigment operator
+console.log(anytype);
+console.log(userName, user_name, UserName);
+console.log(isueserExist, is_ueser_Exist, IsueserExist);
+//console.log(legth)
+//Arrays => [] =>0, ...
+var students = ['hassan', 'ahsan', 'zeeshan', 1, 10, true];
+var Student = students[3];
+console.log(students);
+var firstname = students[0];
+console.log(firstname);
+console.log(students);
+console.log(Student);
+console.log("hello world");
---
+++ ts main.ts
@@ -0,0 +1,24 @@
+ //camelCas
+ //snake_case
+ //Pasalcase
+ let userName ="adnan"; // e.g. camelcase
+ let user_name = "hassan"; // e.g. camelcase
+ let username = "zeeshan"; // e.g.paselcase
+ let isuserExist = true;
+ let is_user_exist = false;
+ let Isuserexist = 1;
+ let anyType;
+ //assigment operator
+ console.log(anyType)t
+ console.log(userName,user_name,userName);
+ console.log(isuserExist, is_user_exist, Isuserexist);
+ //Arrays => [] => 0....
+ let stutendt = ["adnan","hassan","zeeshan"];
+ let firststudentName = stutendt[2];
+ console.log(firststudentName);
+ console.log(stutendt[1], stutendt[2]);
+ let counting = [1,2,3,4,5,6];
+ let truefalse = [true, false];
+ console.log(truefalse[1]);
+ console.log(counting[4]);
+  
